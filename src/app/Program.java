77bf4d8ca05dd6cef2ff7b7ea134aec5838b6626
João Vitor <jvc.lopes@outlook.com>
2024-09-04 16:11:20 -0300

    insert implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,6 +34,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("");
+		
+		System.out.println("=== TEST #4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inseted! New id = " + newSeller.getId());
 	}
 }
