f7cbba28d72d11d111d911fbc40b3326552c6be3
João Vitor <jvc.lopes@outlook.com>
2024-09-04 15:32:11 -0300

    findAll implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -25,6 +25,14 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("");
+		
+		System.out.println("=== TEST #3: seller findAll =====");
+		list = sellerDao.findAll(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -11,6 +11,6 @@ public interface SellerDao {
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer Id);
-	List<Seller> findAll();
+	List<Seller> findAll(Department department);
 	List<Seller> findByDepartment(Department department);
 }
