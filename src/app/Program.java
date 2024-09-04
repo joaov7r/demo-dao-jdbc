bab7465ab1173d7e932b217dd5080a08dc39eb82
João Vitor <jvc.lopes@outlook.com>
2024-09-04 16:26:30 -0300

    update implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -41,5 +41,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inseted! New id = " + newSeller.getId());
+		
+		System.out.println("");
+		
+		System.out.println("=== TEST #5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
